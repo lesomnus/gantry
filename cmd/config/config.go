f27ebdc99b8663512ee3abea5802104ca9d54fb6
Seunghyun Hwang <lesomnus@gmail.com>
2026-07-01 01:42:56 +0900

    rename

--- cmd/config/config.go
+++ cmd/config/config.go
@@ -8,8 +8,8 @@ import (
 )
 
 var DefaultConfigPaths = []string{
-	"go-app.yaml",
-	"go-app.yml",
+	"gantry.yaml",
+	"gantry.yml",
 }
 
 type Config struct {
--- cmd/version.go
+++ cmd/version.go
@@ -3,14 +3,14 @@ package cmd
 import (
 	"context"
 
-	"github.com/lesomnus/go-app/cmd/version"
+	"github.com/lesomnus/gantry/cmd/version"
 	"github.com/lesomnus/xli"
 )
 
 func NewCmdVersion() *xli.Command {
-	const Template = `GO_APP_VERSION=%s
-GO_APP_GIT_REV=%s
-GO_APP_GIT_DIRTY=%v
+	const Template = `GANTRY_VERSION=%s
+GANTRY_GIT_REV=%s
+GANTRY_GIT_DIRTY=%v
 `
 	return &xli.Command{
 		Name:  "version",
